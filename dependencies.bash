90dd9a0e323d0de84d85b09f96eb502e0725b07b
vincenzo-cuprous <159692904+vincenzo-cuprous@users.noreply.github.com>
2024-08-12 19:26:19 +0500

    Create dependencies.bash

---
+++ dependencies.bash
@@ -0,0 +1,7 @@
+#! /bin/bash
+paru -S aur/pyinstaller --needed --noconfirm && 
+    $ paru -S python3 --needed --moconfirm &&
+    $ paru -S rustup && 
+    $ rustup default stable
+    $ python3 -m venv myenv &&
+    $ pip install pyinstaller
